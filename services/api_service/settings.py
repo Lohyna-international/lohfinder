60829a7f1381a9e35efae4e8a13a3a2e9fc594ce
OlegAndrus12 <andrusoleg123@gmail.com>
2021-11-11 17:35:20 +0200

    Added index method

--- services/api_service/settings.py
+++ services/api_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ')(tw9n$_4a2yxjabdqx_&kr&c_h=*m5y=_7)t!#y!6fz8f2q1s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_service'
+    'rest_framework',
+    'api_service',
 ]
 
 MIDDLEWARE = [
--- services/api_service/views.py
+++ services/api_service/views.py
@@ -1,1 +1,12 @@
 #bussines logic will leave there
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def index(response):
+    print("asd  ")
+    context = {
+        "method name" : "method url"
+    }
+    return Response(context) 
