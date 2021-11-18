79879d5f2d46a66e6ebf6c29634426f0ecc1e5c1
Oleh Andrus <o.andrus@ualwo8686nbl.levi9.com>
2021-11-18 14:53:20 +0200

    PubSub layer basic

--- services/user_service/publisher.py
+++ services/user_service/publisher.py
@@ -2,21 +2,21 @@ import os
 import json
 from google.cloud import pubsub_v1
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getcwd() + "/firebase.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getcwd() + "/google_cloud.json"
 
 
-project_id = "lohyna-user-service"
-topic_id = "create_user"
+def test_publisher():
+    project_id = "lohfinder-app"
+    topic_id = "hello_topic"
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(project_id, topic_id)
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_id)
 
-data = {
-    1: "Data for the first method",
-    2: "Data for the second method",
-}
-message = json.dumps(data).encode("utf-8")
+    data = {
+        1: "Data for the first method",
+    }
 
-res = publisher.publish(topic_path, message)
+    message = json.dumps(data).encode("utf-8")
 
-print(f"published message: {res.result()}")
+    res = publisher.publish(topic_path, message)
+    print(f"published message: {res.result()}")
--- services/user_service/subscriber.py
+++ services/user_service/subscriber.py
@@ -1,27 +1,26 @@
 import os
 from google.cloud import pubsub_v1
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getcwd() + "/firebase.json"
-timeout = 2.0 #secs
-
-subscriber = pubsub_v1.SubscriberClient()
-
-subscription_path = 'projects/lohyna-user-service/subscriptions/create_user-sub'
-
-print(f"Listening messages on {subscription_path}")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getcwd() + "/google_cloud.json"
 
 
-def callback(message):
-    print(f"Received message : {message}")
-    print(f"Data : {message.data}")
-    print(type(message))
-    message.ack()
+def test_subscriber():
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = 'projects/lohyna-user-service/subscriptions/create_user-sub'
 
-user_pull = subscriber.subscribe(subscription_path, callback = callback)
+    print(f"Listening messages on {subscription_path}")
 
-with subscriber:
-    try:
-        user_pull.result()
-    except:
-        user_pull.cancel()
-        user_pull.result()
+    def callback(message):
+        print(f"Received message : {message}")
+        print(f"Data : {message.data}")
+        print(type(message))
+        message.ack()
+
+    user_pull = subscriber.subscribe(subscription_path, callback = callback)
+
+    with subscriber:
+        try:
+            user_pull.result()
+        except:
+            user_pull.cancel()
+            user_pull.result()
