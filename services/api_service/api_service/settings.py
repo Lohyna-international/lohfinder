165292e370639d86061a33ffa3a039dfb5b8df0c
OlegAndrus12 <andrusoleg123@gmail.com>
2021-11-18 19:35:50 +0200

    Dockerfile and docker-compose are created. Restructuring project

--- services/api_service/api_service/settings.py
+++ services/api_service/api_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')(tw9n$_4a2yxjabdqx_&kr&c_h=*m5y=_7)t!#y!6fz8f2q1s'
+SECRET_KEY = os.environ.get("SECRET_KEY")  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default = 0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
